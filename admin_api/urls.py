9e0cf404894e6835a533179795c6130f1c9dd1c1
ArunKumar237 <arunchary237@gmail.com>
2024-09-04 13:58:36 +0530

    jwt location changed from admin module to project folder url

--- admin_api/urls.py
+++ admin_api/urls.py
@@ -1,10 +1,6 @@
 from .views import PlanCategoryCRUD, PlanMasterCRUD
 from rest_framework import routers
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 router = routers.DefaultRouter()
 router.register('plancat',PlanCategoryCRUD)
@@ -12,6 +8,4 @@ router.register('planmat',PlanMasterCRUD)
 
 urlpatterns = [
     path('api/',include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
