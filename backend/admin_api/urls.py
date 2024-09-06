f3db8292bd7662ff1b8c322ef84b753e0349cbce
ArunKumar237 <arunchary237@gmail.com>
2024-09-06 13:35:02 +0530

    new table in admin module created

--- backend/admin_api/urls.py
+++ backend/admin_api/urls.py
@@ -1,10 +1,11 @@
-from .views import PlanCategoryCRUD, PlanMasterCRUD
+from .views import PlanCategoryCRUD, PlanMasterCRUD, CaseWorkerAcctCRUD
 from rest_framework import routers
 from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register('plancat',PlanCategoryCRUD)
 router.register('planmat',PlanMasterCRUD)
+router.register('casewrkacct',CaseWorkerAcctCRUD)
 
 urlpatterns = [
     path('',include(router.urls)),
