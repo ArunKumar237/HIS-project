6d3e01324b4603115b029ef7a123d58ef035e82c
ArunKumar237 <arunchary237@gmail.com>
2024-09-06 12:04:59 +0530

    frontend folder created

--- backend/datacol_api/admin.py
+++ backend/datacol_api/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
+from .models import DC_Cases, DC_Income
 
-admin.site.register(DC_Cases)
-admin.site.register(DC_Income)
+class DC_CasesAdmin(admin.ModelAdmin):
+    list_display=['CASE_ID','CASE_NUM','APP_ID','PLAN_ID']
+
+class DC_IncomeAdmin(admin.ModelAdmin):
+    list_display=['INCOME_ID','CASE_NUM','EMP_INCOME','PROPERTY_INCOME']
+
+
+admin.site.register(DC_Cases,DC_CasesAdmin)
+admin.site.register(DC_Income,DC_IncomeAdmin)
