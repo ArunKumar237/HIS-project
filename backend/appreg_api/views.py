107a04016083c04b166335fc42b5764a568f57bd
Madhuri <madhureddy0812@gmail.com>
2024-09-13 17:36:22 +0530

    CrudAPI

--- backend/appreg_api/views.py
+++ backend/appreg_api/views.py
@@ -1,13 +1,47 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.viewsets import ViewSet
 from .serializers import  *
 from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.response import Response
+from rest_framework import status
 
 
 
 # Create your views here.
 
-class appRegModuleModelViewSet(viewsets.ModelViewSet):
-    queryset = appReg_Module.objects.all()
-    serializer_class = appRegSerializers
-    permission_classes=[DjangoModelPermissions,]
+class appRegModuleModelViewSet(ViewSet):
+    def list(self,request):
+        queryset = appReg_Module.objects.all()
+        serializer = appRegSerializers(queryset,many=True)
+        return Response(serializer.data)
+    def create(self,request,*args,**kwargs):
+        serializer = appRegSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def retrieve(self,request,pk=None):
+        try:
+            appReg = appReg_Module.objects.get(pk = pk)
+        except appReg_Module.DoesNotExist:
+            return Response({'error':'Record Not Found'},status=status.HTTP_404_NOT_FOUND)
+    def update(self,request,pk=None):
+        try:
+            appReg = appReg_Module.objects.get(pk=pk)
+        except appReg_Module.DoesNotExist:
+            return Response({'error':'Record Does Not Found'},status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = appRegSerializers(appReg,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+    def destroy(self,request,pk=None):
+        try:
+            appReg = appReg_Module.objects.get(pk=pk)
+        except appReg_Module.DoesNotExist:
+            return Response({'error':'Record Not Found'},status=status.HTTP_404_NOT_FOUND)
+        
+        appReg.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
