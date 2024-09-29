3572f2d8323892551722bc066ce831831af035df
Panuganti Arun Kumar <46347613+ArunKumar237@users.noreply.github.com>
2024-09-29 23:28:32 +0530

    Update settings.py

--- backend/health_insurance_system/settings.py
+++ backend/health_insurance_system/settings.py
@@ -173,7 +173,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Replace with your frontend URL
     "http://127.0.0.1:5173",  # Another common localhost URL
-    "https://his-project-git-main-arunkumar237s-projects.vercel.app/",
+    "https://his-project-git-main-arunkumar237s-projects.vercel.app",
     "https://his-project-eight.vercel.app",
 ]
 
